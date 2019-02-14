74b676352bd8f8db6d04544751855c0b0d04c7a2
Meezaan-ud-Din <meezaan@giffgaff.co.uk>
2019-02-14 16:19:40 +0000

    fixed mounted issue

--- api/startup.sh
+++ api/startup.sh
@@ -4,10 +4,12 @@
 ###########################################################################
 ###########################################################################
 # Move config file to the right place.
+echo "Setting up config and symlinks"
 rm -rf /var/www/html
 ln -sf /var/www/public /var/www/html
 cp -rf /var/www/config.php /var/www/config/api.php
 
+chown -R www-data:www-data /var/www/public/uploads
 ###### DIRECTUS SETUP ENDS - Don't mess with stuff below this line #########
 ###########################################################################
 ###########################################################################
